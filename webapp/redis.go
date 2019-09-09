9005961749479369c500f6f3c7d77b22874b6dcd
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-09-09 20:20:50 +0530

    Added code for redis

--- webapp/redis.go
+++ webapp/redis.go
@@ -1,5 +1,39 @@
 package webapp
 
 import (
-	
+    log "github.com/sirupsen/logrus"
+    "fmt"
+    "io"
+    "gopkg.in/ini.v1"
+	"os"
+	"github.com/gomodule/redigo/redis"	
 )
+
+initializeCache() {
+	propertyfile := "/etc/conf.d/ot-go-webapp/application.ini"
+
+    if fileExists(propertyfile) {
+        loggingInit()
+        vaules, err := ini.Load(propertyfile)
+        if err != nil {
+            log.Error("No property file found in " + propertyfile)
+        }
+        redisHost = vaules.Section("redis").Key("REDIS_HOST").String()
+        redisPort = vaules.Section("redis").Key("REDIS_PORT").String()
+        log.Info("Reading properties file " + propertyfile)
+    } else {
+        redisHost = os.Getenv("REDIS_HOST")
+        redisPort = os.Getenv("REDIS_PORT")
+        loggingInit()
+        log.Info("No property file found, using environment variables")
+	}
+
+	pool := &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", redisHost + ":" + "6379")
+		},
+	}
+	return pool
+}
