69176d1575d4891cd831d9a551a2bde03577e422
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-09-16 18:38:11 +0530

    Updated ode

--- webapp/main.go
+++ webapp/main.go
@@ -4,15 +4,37 @@ import (
     "net/http"
     dbcheck "github.com/dimiro1/health/db"
     "github.com/dimiro1/health"
+    "github.com/dimiro1/health/redis"
+    "gopkg.in/ini.v1"
 )
 
 func Run() {
+
+	propertyfile := "/etc/conf.d/ot-go-webapp/application.ini"
+
+    if fileExists(propertyfile) {
+        loggingInit()
+        vaules, err := ini.Load(propertyfile)
+        if err != nil {
+            log.Error("No property file found in " + propertyfile)
+        }
+        redisHost := vaules.Section("redis").Key("REDIS_HOST").String()
+        redisPort := vaules.Section("redis").Key("REDIS_PORT").String()
+        log.Info("Reading properties file " + propertyfile)
+    } else {
+        redisHost := os.Getenv("REDIS_HOST")
+        redisPort := os.Getenv("REDIS_PORT")
+        loggingInit()
+        log.Info("No property file found, using environment variables")
+    }
+
     generateLogging()
     createDatabaseTable()
     db := dbConn()
     mysql := dbcheck.NewMySQLChecker(db)
     handler := health.NewHandler()
     handler.AddChecker("MySQL", mysql)
+    handler.AddChecker("Redis", redis.NewChecker("tcp", redisHost + ":" + redisPort))
     http.Handle("/health", handler)
     http.HandleFunc("/", Index)
     http.HandleFunc("/signup", signUp)
@@ -22,14 +44,5 @@ func Run() {
     http.HandleFunc("/insert", Insert)
     http.HandleFunc("/update", Update)
     http.HandleFunc("/delete", Delete)
-    // handler.AddChecker("MySQL", mysql)
-    // http.Handle("/health", handler)
-    // http.HandleFunc("/", redisIndex)
-    // http.HandleFunc("/show", redisUserShow)
-    // http.HandleFunc("/new", New)
-    // http.HandleFunc("/edit", redisEditUser)
-    // http.HandleFunc("/insert", redisInsertUser)
-    // http.HandleFunc("/update", redisUpdateUser)
-    // http.HandleFunc("/delete", redisDeleteUser)
     http.ListenAndServe(":8080", nil)
 }
--- webapp/redis.go
+++ webapp/redis.go
@@ -60,203 +60,162 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// func Signin(w http.ResponseWriter, r *http.Request) {
-// 	var creds Credentials
-// 	// Get the JSON body and decode into credentials
-// 	err := json.NewDecoder(r.Body).Decode(&creds)
-// 	if err != nil {
-// 		// If the structure of the body is wrong, return an HTTP error
-// 		w.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
+func redisIndex(w http.ResponseWriter, r *http.Request) {
+	pool = initializeCache()
+	conn :=  pool.Get()
+    emp := Employee{}
+	res := []Employee{}
+	keys_list, err := redis.Strings(conn.Do("KEYS", "*"))
+	if err != nil {
+		log.Error(err)
+	}
 
-// 	// Get the expected password from our in memory map
-// 	expectedPassword, ok := users[creds.Username]
+	for _, key := range keys_list {
+		name, err := redis.String(conn.Do("HGET", key, "name"))
+		if err != nil {
+			log.Error(err)
+		}
+		email, err := redis.String(conn.Do("HGET", key, "email"))
+		if err != nil {
+			log.Error(err)
+		}
+		date, err := redis.String(conn.Do("HGET", key, "date"))
+		if err != nil {
+			log.Error(err)
+		}
+		city, err := redis.String(conn.Do("HGET", key, "city"))
+		if err != nil {
+			log.Error(err)
+		}
+		id, err := strconv.Atoi(key)
+		if err != nil {
+			log.Error(err)
+		}
+        emp.Id = id
+        emp.Name = name
+        emp.Email = email
+        emp.Date = date
+		emp.City = city
+		res = append(res, emp)
+	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+}
 
-// 	// If a password exists for the given user
-// 	// AND, if it is the same as the password we received, the we can move ahead
-// 	// if NOT, then we return an "Unauthorized" status
-// 	if !ok || expectedPassword != creds.Password {
-// 		w.WriteHeader(http.StatusUnauthorized)
-// 		return
-// 	}
+func redisUserShow(w http.ResponseWriter, r *http.Request) {
+	pool = initializeCache()
+	conn :=  pool.Get()
+	nId := r.FormValue("id")
+	emp := Employee{}
+	name, err := redis.String(conn.Do("HGET", nId, "name"))
+	if err != nil {
+		log.Error(err)
+	}
+	email, err := redis.String(conn.Do("HGET", nId, "email"))
+	if err != nil {
+		log.Error(err)
+	}
+	date, err := redis.String(conn.Do("HGET", nId, "date"))
+	if err != nil {
+		log.Error(err)
+	}
+	city, err := redis.String(conn.Do("HGET", nId, "city"))
+	if err != nil {
+		log.Error(err)
+	}
+	id, err := strconv.Atoi(nId)
+	if err != nil {
+		log.Error(err)
+	}
+	emp.Id = id
+	emp.Name = name
+	emp.Email = email
+	emp.Date = date
+	emp.City = city
 
-// 	// Create a new random session token
-// 	sessionToken := uuid.NewV4().String()
-// 	// Set the token in the cache, along with the user whom it represents
-// 	// The token has an expiry time of 120 seconds
-// 	_, err = cache.Do("SETEX", sessionToken, "120", creds.Username)
-// 	if err != nil {
-// 		// If there is an error in setting the cache, return an internal server error
-// 		w.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
+	tmpl.ExecuteTemplate(w, "Show", emp)
+}
 
-// 	// Finally, we set the client cookie for "session_token" as the session token we just generated
-// 	// we also set an expiry time of 120 seconds, the same as the cache
-// 	http.SetCookie(w, &http.Cookie{
-// 		Name:    "session_token",
-// 		Value:   sessionToken,
-// 		Expires: time.Now().Add(120 * time.Second),
-// 	})
-// }
+func redisEditUser(w http.ResponseWriter, r *http.Request) {
+	pool = initializeCache()
+	conn :=  pool.Get()
+	nId := r.FormValue("id")
+	emp := Employee{}
+	name, err := redis.String(conn.Do("HGET", nId, "name"))
+	if err != nil {
+		log.Error(err)
+	}
+	email, err := redis.String(conn.Do("HGET", nId, "email"))
+	if err != nil {
+		log.Error(err)
+	}
+	date, err := redis.String(conn.Do("HGET", nId, "date"))
+	if err != nil {
+		log.Error(err)
+	}
+	city, err := redis.String(conn.Do("HGET", nId, "city"))
+	if err != nil {
+		log.Error(err)
+	}
+	id, err := strconv.Atoi(nId)
+	if err != nil {
+		log.Error(err)
+	}
+	emp.Id = id
+	emp.Name = name
+	emp.Email = email
+	emp.Date = date
+	emp.City = city
 
-// func redisIndex(w http.ResponseWriter, r *http.Request) {
-// 	pool = initializeCache()
-// 	conn :=  pool.Get()
-//     emp := Employee{}
-// 	res := []Employee{}
-// 	keys_list, err := redis.Strings(conn.Do("KEYS", "*"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
+	tmpl.ExecuteTemplate(w, "Edit", emp)
+}
 
-// 	for _, key := range keys_list {
-// 		name, err := redis.String(conn.Do("HGET", key, "name"))
-// 		if err != nil {
-// 			log.Error(err)
-// 		}
-// 		email, err := redis.String(conn.Do("HGET", key, "email"))
-// 		if err != nil {
-// 			log.Error(err)
-// 		}
-// 		date, err := redis.String(conn.Do("HGET", key, "date"))
-// 		if err != nil {
-// 			log.Error(err)
-// 		}
-// 		city, err := redis.String(conn.Do("HGET", key, "city"))
-// 		if err != nil {
-// 			log.Error(err)
-// 		}
-// 		id, err := strconv.Atoi(key)
-// 		if err != nil {
-// 			log.Error(err)
-// 		}
-//         emp.Id = id
-//         emp.Name = name
-//         emp.Email = email
-//         emp.Date = date
-// 		emp.City = city
-// 		res = append(res, emp)
-// 	}
-// 	tmpl.ExecuteTemplate(w, "Index", res)
-// }
+func redisInsertUser(w http.ResponseWriter, r *http.Request) {
+	pool = initializeCache()
+	conn :=  pool.Get()
+	if r.Method == "POST" {
+		nId := r.FormValue("id")
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		email := r.FormValue("email")
+		date := r.FormValue("date")
 
-// func redisUserShow(w http.ResponseWriter, r *http.Request) {
-// 	pool = initializeCache()
-// 	conn :=  pool.Get()
-// 	nId := r.FormValue("id")
-// 	emp := Employee{}
-// 	name, err := redis.String(conn.Do("HGET", nId, "name"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	email, err := redis.String(conn.Do("HGET", nId, "email"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	date, err := redis.String(conn.Do("HGET", nId, "date"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	city, err := redis.String(conn.Do("HGET", nId, "city"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	id, err := strconv.Atoi(nId)
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	emp.Id = id
-// 	emp.Name = name
-// 	emp.Email = email
-// 	emp.Date = date
-// 	emp.City = city
+		fmt.Println(nId)
+		insForm, err := conn.Do("HMSET", nId, "name", name, "email", email, "date", date, "city", city)
+		if err != nil {
+			log.Error(err)
+		}
+		fmt.Print(insForm)
+	}
+	http.Redirect(w, r, "/", 301)
+}
 
-// 	tmpl.ExecuteTemplate(w, "Show", emp)
-// }
+func redisUpdateUser(w http.ResponseWriter, r *http.Request) {
+	pool = initializeCache()
+	conn :=  pool.Get()
+	if r.Method == "POST" {
+		nId := r.FormValue("id")
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		email := r.FormValue("email")
+		date := r.FormValue("date")
+		insForm, err := conn.Do("HMSET", nId, "name", name, "email", email, "date", date, "city", city)
+		if err != nil {
+			log.Error(err)
+		}
+		fmt.Print(insForm)
+	}
+	http.Redirect(w, r, "/", 301)
+}
 
-// func redisEditUser(w http.ResponseWriter, r *http.Request) {
-// 	pool = initializeCache()
-// 	conn :=  pool.Get()
-// 	nId := r.FormValue("id")
-// 	emp := Employee{}
-// 	name, err := redis.String(conn.Do("HGET", nId, "name"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	email, err := redis.String(conn.Do("HGET", nId, "email"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	date, err := redis.String(conn.Do("HGET", nId, "date"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	city, err := redis.String(conn.Do("HGET", nId, "city"))
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	id, err := strconv.Atoi(nId)
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	emp.Id = id
-// 	emp.Name = name
-// 	emp.Email = email
-// 	emp.Date = date
-// 	emp.City = city
-
-// 	tmpl.ExecuteTemplate(w, "Edit", emp)
-// }
-
-// func redisInsertUser(w http.ResponseWriter, r *http.Request) {
-// 	pool = initializeCache()
-// 	conn :=  pool.Get()
-// 	if r.Method == "POST" {
-// 		nId := r.FormValue("id")
-// 		name := r.FormValue("name")
-// 		city := r.FormValue("city")
-// 		email := r.FormValue("email")
-// 		date := r.FormValue("date")
-
-// 		fmt.Println(nId)
-// 		insForm, err := conn.Do("HMSET", nId, "name", name, "email", email, "date", date, "city", city)
-// 		if err != nil {
-// 			log.Error(err)
-// 		}
-// 		fmt.Print(insForm)
-// 	}
-// 	http.Redirect(w, r, "/", 301)
-// }
-
-// func redisUpdateUser(w http.ResponseWriter, r *http.Request) {
-// 	pool = initializeCache()
-// 	conn :=  pool.Get()
-// 	if r.Method == "POST" {
-// 		nId := r.FormValue("id")
-// 		name := r.FormValue("name")
-// 		city := r.FormValue("city")
-// 		email := r.FormValue("email")
-// 		date := r.FormValue("date")
-// 		insForm, err := conn.Do("HMSET", nId, "name", name, "email", email, "date", date, "city", city)
-// 		if err != nil {
-// 			log.Error(err)
-// 		}
-// 		fmt.Print(insForm)
-// 	}
-// 	http.Redirect(w, r, "/", 301)
-// }
-
-// func redisDeleteUser(w http.ResponseWriter, r *http.Request) {
-// 	pool = initializeCache()
-// 	conn :=  pool.Get()
-// 	nId := r.FormValue("id")
-// 	insForm, err := conn.Do("DEL", nId)
-// 	if err != nil {
-// 		log.Error(err)
-// 	}
-// 	fmt.Print(insForm)
-// 	fmt.Println(insForm)
-// 	http.Redirect(w, r, "/", 301)
-// }
+func redisDeleteUser(w http.ResponseWriter, r *http.Request) {
+	pool = initializeCache()
+	conn :=  pool.Get()
+	nId := r.FormValue("id")
+	insForm, err := conn.Do("DEL", nId)
+	if err != nil {
+		log.Error(err)
+	}
+	fmt.Print(insForm)
+	fmt.Println(insForm)
+	http.Redirect(w, r, "/", 301)
+}
