2e55a5145fe668dd71d719d35903de75e0c6c0c4
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-07-05 14:34:12 +0530

    Completed frontend

--- frontend/src/EmployeeData.js
+++ frontend/src/EmployeeData.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import react, * as React from "react";
-import {Grid, StatsCard} from 'tabler-react';
+import {Grid, StatsCard, Card, colors} from 'tabler-react';
+import C3Chart from "react-c3js";
 
 function generateResult(input) {
   if (input === undefined) {
@@ -85,6 +86,178 @@ export function ListEmployeeInActiveEmployee() {
       total={generateResult(empData["Ex-Employee"])}
       label="Ex-Employees"
     />
+    </Grid.Col>
+  )
+}
+
+export function RoleDistribution() {
+  const [stats, handleStats] = useState([]);
+
+  const FetchData = async () => {
+    const data = await fetch(process.env.REACT_APP_GATEWAY_URL + "/management/search/roles");
+    const stats = await data.json();
+    handleStats(stats)  
+  }
+
+  useEffect(() => {
+    FetchData()
+  }, [])
+  const empData = stats
+
+  return (
+      <Grid.Col sm={4}>
+      <Card>
+        <Card.Header>
+          <Card.Title>Job Role Distribution</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <C3Chart
+            style={{ height: "12rem" }}
+            data={{
+              columns: [
+                // each columns data
+                ["DevOps", generateResult(empData["DevOps"])],
+                ["Developer", generateResult(empData["Developer"])],
+              ],
+              type: "donut", // default type of chart
+              colors: {
+                data1: colors["green"],
+                data2: colors["green-light"],
+              },
+              names: {
+                // name of each serie
+                data1: "Maximum",
+                data2: "Minimum",
+              },
+            }}
+            legend={{
+              show: false, //hide legend
+            }}
+            padding={{
+              bottom: 0,
+              top: 0,
+            }}
+          />
+        </Card.Body>
+      </Card>
+    </Grid.Col>
+  )
+}
+
+export function LocationDistribution() {
+  const [stats, handleStats] = useState([]);
+
+  const FetchData = async () => {
+    const data = await fetch(process.env.REACT_APP_GATEWAY_URL + "/management/search/location");
+    const stats = await data.json();
+    handleStats(stats)  
+  }
+
+  useEffect(() => {
+    FetchData()
+  }, [])
+  const empData = stats
+
+  return (
+    <Grid.Col sm={4}>
+    <Card>
+      <Card.Header>
+        <Card.Title>Locations Distribution</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <C3Chart
+          style={{ height: "12rem" }}
+          data={{
+            columns: [
+              // each columns data
+              ["Delhi", generateResult(empData["Delhi"])],
+              ["Bangalore", generateResult(empData["Bangalore"])],
+              ["Hyederabad", generateResult(empData["Hyderabad"])],
+              ["Newyork", generateResult(empData["Newyork"])],
+            ],
+            type: "donut", // default type of chart
+            colors: {
+              data1: colors["blue-darker"],
+              data2: colors["blue"],
+              data3: colors["blue-light"],
+              data4: colors["blue-lighter"],
+            },
+            names: {
+              // name of each serie
+              data1: "A",
+              data2: "B",
+              data3: "C",
+              data4: "D",
+            },
+          }}
+          legend={{
+            show: false, //hide legend
+          }}
+          padding={{
+            bottom: 0,
+            top: 0,
+          }}
+        />
+      </Card.Body>
+    </Card>
+  </Grid.Col>
+  )
+}
+
+export function StatusDistribution() {
+  const [stats, handleStats] = useState([]);
+
+  const FetchData = async () => {
+    const data = await fetch(process.env.REACT_APP_GATEWAY_URL + "/management/search/status");
+    const stats = await data.json();
+    handleStats(stats)  
+  }
+
+  useEffect(() => {
+    FetchData()
+  }, [])
+  const empData = stats
+
+  return (
+      <Grid.Col sm={4}>
+      <Card>
+        <Card.Header>
+          <Card.Title>Employees Distribution</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <C3Chart
+            style={{ height: "12rem" }}
+            data={{
+              columns: [
+                // each columns data
+                ["Current Employees", generateResult(empData["Current Employee"])],
+                ["Ex-Employees", generateResult(empData["Ex-Employee"])],
+              ],
+              type: "donut", // default type of chart
+              colors: {
+                data1: colors["blue-darker"],
+                data2: colors["blue"],
+                data3: colors["blue-light"],
+                data4: colors["blue-lighter"],
+              },
+              names: {
+                // name of each serie
+                data1: "A",
+                data2: "B",
+                data3: "C",
+                data4: "D",
+              },
+            }}
+            legend={{
+              show: false, //hide legend
+            }}
+            padding={{
+              bottom: 0,
+              top: 0,
+            }}
+          />
+        </Card.Body>
+      </Card>
     </Grid.Col>
   )
 }
--- frontend/src/HomePage.react.js
+++ frontend/src/HomePage.react.js
@@ -26,7 +26,10 @@ import C3Chart from "react-c3js";
 import SiteWrapper from "./SiteWrapper.react";
 import { ListAllEmployees, 
   ListEmployeeActiveEmployee,
-  ListEmployeeInActiveEmployee } from './EmployeeData';
+  ListEmployeeInActiveEmployee,
+  RoleDistribution,
+  LocationDistribution,
+  StatusDistribution } from './EmployeeData';
 
 function Home() {
   return (
@@ -36,14 +39,6 @@ function Home() {
           <ListAllEmployees/>
           <ListEmployeeActiveEmployee/>
           <ListEmployeeInActiveEmployee/>
-          {/* <Grid.Col sm={3}>
-            <StatsCard
-              layout={1}
-              movement={0}
-              total="20"
-              label="Ex-Employees"
-            />
-          </Grid.Col> */}
           <Grid.Col sm={3}>
             <StatsCard
               layout={1}
@@ -54,124 +49,9 @@ function Home() {
           </Grid.Col>
           <Grid.Col>
             <Grid.Row cards="true">
-              <Grid.Col sm={4}>
-                <Card>
-                  <Card.Header>
-                    <Card.Title>Job Role Distribution</Card.Title>
-                  </Card.Header>
-                  <Card.Body>
-                    <C3Chart
-                      style={{ height: "12rem" }}
-                      data={{
-                        columns: [
-                          // each columns data
-                          ["DevOps", 100],
-                          ["Developer", 30],
-                        ],
-                        type: "donut", // default type of chart
-                        colors: {
-                          data1: colors["green"],
-                          data2: colors["green-light"],
-                        },
-                        names: {
-                          // name of each serie
-                          data1: "Maximum",
-                          data2: "Minimum",
-                        },
-                      }}
-                      legend={{
-                        show: false, //hide legend
-                      }}
-                      padding={{
-                        bottom: 0,
-                        top: 0,
-                      }}
-                    />
-                  </Card.Body>
-                </Card>
-              </Grid.Col>
-              <Grid.Col sm={4}>
-                <Card>
-                  <Card.Header>
-                    <Card.Title>Locations Distribution</Card.Title>
-                  </Card.Header>
-                  <Card.Body>
-                    <C3Chart
-                      style={{ height: "12rem" }}
-                      data={{
-                        columns: [
-                          // each columns data
-                          ["Delhi", 63],
-                          ["Bangalore", 44],
-                          ["Hyederabad", 12],
-                          ["Newyork", 14],
-                        ],
-                        type: "donut", // default type of chart
-                        colors: {
-                          data1: colors["blue-darker"],
-                          data2: colors["blue"],
-                          data3: colors["blue-light"],
-                          data4: colors["blue-lighter"],
-                        },
-                        names: {
-                          // name of each serie
-                          data1: "A",
-                          data2: "B",
-                          data3: "C",
-                          data4: "D",
-                        },
-                      }}
-                      legend={{
-                        show: false, //hide legend
-                      }}
-                      padding={{
-                        bottom: 0,
-                        top: 0,
-                      }}
-                    />
-                  </Card.Body>
-                </Card>
-              </Grid.Col>
-              <Grid.Col sm={4}>
-                <Card>
-                  <Card.Header>
-                    <Card.Title>Employees Distribution</Card.Title>
-                  </Card.Header>
-                  <Card.Body>
-                    <C3Chart
-                      style={{ height: "12rem" }}
-                      data={{
-                        columns: [
-                          // each columns data
-                          ["Current Employees", 130],
-                          ["Ex-Employees", 20],
-                        ],
-                        type: "donut", // default type of chart
-                        colors: {
-                          data1: colors["blue-darker"],
-                          data2: colors["blue"],
-                          data3: colors["blue-light"],
-                          data4: colors["blue-lighter"],
-                        },
-                        names: {
-                          // name of each serie
-                          data1: "A",
-                          data2: "B",
-                          data3: "C",
-                          data4: "D",
-                        },
-                      }}
-                      legend={{
-                        show: false, //hide legend
-                      }}
-                      padding={{
-                        bottom: 0,
-                        top: 0,
-                      }}
-                    />
-                  </Card.Body>
-                </Card>
-              </Grid.Col>
+              <RoleDistribution/>
+              <StatusDistribution/>
+              <LocationDistribution/>
             </Grid.Row>
           </Grid.Col>
         </Grid.Row>
